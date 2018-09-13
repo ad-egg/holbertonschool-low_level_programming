3ddaab2261cecc04c3f56053090be1aaae696a12
Athena Deng <539@holbertonschool.com>
2018-09-13 22:42:30 +0000

    first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char no;
+
+	for (no = '0' ; no <= '9' ; no++)
+	{
+		putchar(no);
+	}
+	putchar ('\n');
+
+	return (0);
+}
