dd3f3b54af25215a2ef146395e1d253ca19228ae
Athena Deng <539@holbertonschool.com>
2018-09-13 04:20:32 +0000

    fixed variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: \n", sizeof(char));
 	printf("Size of an int: \n", sizeof(int));
-	printf("Size of a long int: \n", sizeof(long));
-	printf("Size of a long long int: \n", sizeof(long long));
+	printf("Size of a long int: \n", sizeof(long int));
+	printf("Size of a long long int: \n", sizeof(long long int));
 	printf("Size of a float: \n", sizeof(float));
 	return (0);
 }
