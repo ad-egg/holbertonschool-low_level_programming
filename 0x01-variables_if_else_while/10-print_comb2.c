e81413a7fc6cdb51dfaa8a8374d3efc814c910a5
Athena Deng <539@holbertonschool.com>
2018-09-14 02:56:49 +0000

    first commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0 ; no < 100 ; no++)
+	{
+		putchar((no/10) + '0');
+		putchar((no%10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar ('\n');
+
+	return (0);
+}
