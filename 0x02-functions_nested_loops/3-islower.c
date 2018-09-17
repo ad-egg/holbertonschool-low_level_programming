7b95481946deb73761de6d70efa3a56da93fc330
Athena Deng <539@holbertonschool.com>
2018-09-17 23:37:12 +0000

    first commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c);
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+	return (0);
+}
