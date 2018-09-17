5aae2781f98b900af2e091059076e5a55da75a0a
Athena Deng <539@holbertonschool.com>
2018-09-17 23:11:00 +0000

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char az;
+	for (az = 0; az <= 'z'; az++)
+	{
+		_putchar(az);
+	}
+	_putchar('\n');
+
+	return (0);
+}
