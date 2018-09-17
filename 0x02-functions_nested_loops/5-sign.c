4ef659364e319a02030754277fec8ba90015289c
Athena Deng <539@holbertonschool.com>
2018-09-17 23:58:48 +0000

    first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n);
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+
+	return(0);
+}
