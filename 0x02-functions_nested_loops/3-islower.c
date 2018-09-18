6d644719728c74dc31b5960e999800f203185b05
Athena Deng <539@holbertonschool.com>
2018-09-18 17:48:20 +0000

    removed extra single quotes from return value

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,15 +8,14 @@
 
 int _islower(int c)
 {
-	int c;
 
 	if (c >= 'a' && c <= 'z')
 	{
-		_putchar('1');
+		return (1);
 	}
 	else
 	{
-		_putchar('0');
+		return (0);
 	}
 	_putchar('\n');
 	return (0);
