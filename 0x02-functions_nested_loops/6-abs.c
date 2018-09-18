1763c35917ce64a1e78b6767eeb76400ac502bf0
Athena Deng <539@holbertonschool.com>
2018-09-18 00:04:25 +0000

    first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int);
+{
+	int no;
+
+	if (no > 0)
+		_putchar(no);
+	else if (no == 0)
+		_putchar(no);
+	else
+		_putchar(-no);
+
+	return(0);
+}
