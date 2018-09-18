a1d8d310df04ab4f820674ae53865086feb1f02e
Athena Deng <539@holbertonschool.com>
2018-09-18 19:06:13 +0000

    updated comments

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet - Entry point
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet_x10 - Entry point
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
