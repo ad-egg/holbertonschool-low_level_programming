902368ec16e7dcf2186a7b97d62b933d6d215dca
Athena Deng <539@holbertonschool.com>
2018-09-18 23:14:52 +0000

    changed return value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,28 +4,21 @@
  * _abs - Entry point
  *
  * @no: the absolute value of this variable is displayed
+ *
  * Return: Always 0 (Success)
  */
 
-int _abs(int)
+int _abs(int no)
 {
-	int no;
-
-	if (no > 0)
+	if (no >= 0)
 	{
-		_putchar(no);
-		_putchar('\n');
-	}
-	else if (no == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
+		return (no);
 	}
 	else
 	{
-		_putchar(-no);
-		_putchar('\n');
+		return (no * -1);
 	}
+	_putchar('\n');
 
 	return (0);
 }
