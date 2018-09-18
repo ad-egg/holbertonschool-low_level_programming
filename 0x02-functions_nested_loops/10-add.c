c92b479090348f0880d5fffb0c6b6e3b15fd186c
Athena Deng <539@holbertonschool.com>
2018-09-18 23:21:35 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: this variable will be added to another variable
+ * @b: this variable is added to another variable
+ *
+ * Return: the sum of int a and int b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+	_putchar('\n');
+
+}
