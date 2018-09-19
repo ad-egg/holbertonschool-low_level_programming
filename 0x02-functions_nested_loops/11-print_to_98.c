182b20aeb933fb764c45c34d08d659dba1870472
Athena Deng <539@holbertonschool.com>
2018-09-19 02:41:17 +0000

    incomplete work

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: this integer will be counted from to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		putchar(',');
+		putchar(' ');
+
+
