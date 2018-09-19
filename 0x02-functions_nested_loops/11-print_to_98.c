92faa6262d438f66673a99a65420d3a11db03ba4
Athena Deng <539@holbertonschool.com>
2018-09-19 23:11:29 +0000

    completed writing this function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,23 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n == 98)
 	{
-		putchar(',');
-		putchar(' ');
-
-
+		printf("98");
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("98");
+	}
+	putchar('\n');
+}
