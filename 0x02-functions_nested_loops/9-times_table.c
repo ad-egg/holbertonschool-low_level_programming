bd05c3f37a80501d16adc8ca4d8d1ac3f83b5d89
Athena Deng <539@holbertonschool.com>
2018-09-19 00:54:54 +0000

    added spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,35 @@ void times_table(void)
 	int hor;
 	int ver;
 
-	for(ver = 0; ver <= 9; ver++)
+	for (ver = 0; ver <= 9; ver++)
 	{
 		for (hor = 0; hor <= 9; hor++)
 		{
-			_putchar(((ver * hor) / 10) + '0');
-			_putchar(((ver * hor) % 10) + '0');
-			if (hor < 9)
+			if ((ver * hor) <= 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((ver * hor) + '0');
+				if (hor < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+
+			}
+			else
+			{
+				_putchar(((ver * hor) / 10) + '0');
+				_putchar(((ver * hor) % 10) + '0');
+				if (hor < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
