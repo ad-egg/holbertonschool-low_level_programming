a9440cbeb5fed904e39166272712d546467de3d7
Athena Deng <539@holbertonschool.com>
2018-09-20 19:41:30 +0000

    added description for return value of function

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,8 @@
  * _isupper - checks if c is an uppercase letter or not
  *
  * @c: this variable is checked for whether or not it is an uppercase letter
+ *
+ * Return: 1 if c is uppercase letter, 0 if not
  */
 
 int _isupper(int c)
