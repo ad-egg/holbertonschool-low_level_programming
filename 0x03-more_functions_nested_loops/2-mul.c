631ba7557be1bb3c31e5d865976573540175e7b8
Athena Deng <539@holbertonschool.com>
2018-09-20 20:00:17 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - this function multiplies a and b
+ *
+ * @a: this integer will be multiplied by b
+ * @b: this integer will be multiplied by a
+ *
+ * Return: 0 on success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
