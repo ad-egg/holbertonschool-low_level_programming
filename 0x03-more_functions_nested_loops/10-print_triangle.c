7db1de7b1e805b7ed5820707a34b42b4b6926f44
Athena Deng <539@holbertonschool.com>
2018-09-21 03:05:55 +0000

    incomplete work

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: triangle will be this size
+ */
+
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int space;
+	int hasht;
+
+	
+
+	if (size == 0)
+		_putchar('\n');
+	else
+	{
+		for (y = 1; y < size; y++)
+		{
+			for (x = 1; x < size; x++)
+			{
+				for (space = n; space < y; space --)
+
+		_putchar (32);
+		_putchar(35);
+		_putchar('\n');
+	}
+}
