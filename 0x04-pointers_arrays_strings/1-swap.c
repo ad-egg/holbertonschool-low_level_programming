2e04b71e885a077c98b59641092effe1525ab4eb
Athena Deng <539@holbertonschool.com>
2018-09-24 22:43:20 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps values of two integers
+ *
+ * @*a: its value will be swapped with pointer b
+ * @*b: its value will be swapped with pointer a
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *x = *a;
+
+	*a = *b;
+	*b = *x;
+}
