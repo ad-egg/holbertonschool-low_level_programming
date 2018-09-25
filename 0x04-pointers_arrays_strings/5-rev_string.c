12f0ab14dc4f40d4cead8967c20f1d522eddec64
Athena Deng <539@holbertonschool.com>
2018-09-25 17:57:48 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @*s: the string at this pointer gets reversed
+ */
+
+void rev_string(char *s)
+{
+	int regular;
+	int reverse;
+	int n;
+	char *original;
+
+	regular = 0;
+	reverse = 0;
+	*original = s;
+
+	while (*s != '\0')
+	{
+		reverse++;
+		s++;
+	}
+	reverse--;
+	while (regular <= reverse)
+	{
+		n = *(original + regular);
+		*(original + regular) = *(original + reverse);
+		*(original + reverse) = n;
+		regular++;
+		reverse--;
+	}
+}
