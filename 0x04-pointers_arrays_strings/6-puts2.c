7dcefd564c6134b7cb56674b2106942046633b52
Athena Deng <539@holbertonschool.com>
2018-09-25 18:05:12 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints one char out of two of a string
+ *
+ * @*str: every other letter of this string will be printed
+ */
+
+void puts2(char *str)
+{
+	int ind;
+
+	ind = 1;
+
+	while (*str != '\0')
+	{
+		if ((ind % 2) != 0)
+		{
+			_putchar(*str);
+		}
+		ind++;
+		str++;
+	}
+	_putchar('\n');
+}
