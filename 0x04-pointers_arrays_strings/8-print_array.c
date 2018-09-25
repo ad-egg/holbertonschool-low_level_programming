afdf191063e936762273cece60edeca69acd4435
Athena Deng <539@holbertonschool.com>
2018-09-25 20:36:43 +0000

    updated variable description

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
+ * print_array - prints n elements of an array of integers
  *
- *
- *
+ * @a: array that will have n elements printed
+ * @n: number of elements of the array that will be printed
  */
 
 void print_array(int *a, int n)
