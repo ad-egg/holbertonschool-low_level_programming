841a5139f1a1d4c9532c5059aa59c8b856682808
Athena Deng <539@holbertonschool.com>
2018-09-25 17:50:49 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @*s: pointer for the string
+ */
+
+void print_rev(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+
+	while (*s != '\0')
+	{
+		strlen++;
+		s++;
+	}
+	while (strlen != 0)
+	{
+		_putchar(*s);
+		strlen--;
+		s--;
+	}
+	_putchar('\n');
+}
