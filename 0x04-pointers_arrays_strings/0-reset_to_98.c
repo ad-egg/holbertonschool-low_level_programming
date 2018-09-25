1e9fbd34c8956c3d1afc03e8da89740d6fd83214
Athena Deng <539@holbertonschool.com>
2018-09-25 18:21:37 +0000

    updated variable description

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - resets value of variable to 98
  *
- * @*n: this pointer's value will be changed to 98
+ * @n: this pointer's value will be changed to 98
  */
 
 void reset_to_98(int *n)
