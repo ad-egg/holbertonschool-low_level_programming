d5f4141e92eef8a2950c02e94a19acd94d48985e
Athena Deng <539@holbertonschool.com>
2018-09-26 02:51:52 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string pointed to by src including the null byte
+ *
+ * @dest: the string will be printed to this buffer
+ * @src: points out the string to be copied
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	
