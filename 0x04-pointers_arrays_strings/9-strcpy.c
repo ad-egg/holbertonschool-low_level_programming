4576fc8b3719410e296ae5a9393adfd27eebbbda
Athena Deng <539@holbertonschool.com>
2018-09-26 20:05:30 +0000

    added loops

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -11,4 +11,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	
+	int len;
+	int n;
+
+	n = 0;
+
+	for (len = 0; src[len] != '\0'; len++);
+	while (n < len)
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	return (*dest);
+}
