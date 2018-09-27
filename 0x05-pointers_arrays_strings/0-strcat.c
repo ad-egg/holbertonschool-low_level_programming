633b0e632ef3afe185e5f098bee8f6b6e1800122
Athena Deng <539@holbertonschool.com>
2018-09-27 23:27:21 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: src string will be appended to this string
+ * @src: this string will be appended to dest string
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *pt = dest;
+
+	while(*dest != '\0')
+		dest++;
+	while(*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (pt);
+}
