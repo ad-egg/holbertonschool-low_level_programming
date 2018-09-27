003c8e1022033be1415dcc44a8ee0e97b1e80d88
Athena Deng <539@holbertonschool.com>
2018-09-27 00:42:18 +0000

    first commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 12; n <= 789; n++)
+	{
+		if (((n % 10) > ((n / 10) % 10)) && (((n / 10) % 10) > (n / 100)))
+		{
+			putchar ((n / 100) + '0');
+			if ((n / 100) != ((n / 10) % 10))
+			{
+				putchar(((n / 10) % 10) + '0');
+				if (((n / 10) % 10) != (n % 10))
+					putchar((n % 10) + '0');
+				else
+					continue;
+			}
+			else
+				continue;
+			if (n < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
