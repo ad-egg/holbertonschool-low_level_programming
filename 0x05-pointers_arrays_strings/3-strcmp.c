bb68a30d29c1a69338968688c8a64a108043357f
Athena Deng <539@holbertonschool.com>
2018-09-28 03:56:24 +0000

    incomplete work

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -11,18 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1;
-	int len2;
-
-	len1 = 0;
-	len2 = 0;
-
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-	if (len1 == len2)
+	while ((*s1 == *s2) && (*s1 != '\0'') && (s2 != '\0'))
+	{
+		s1++;
+		s2++;
+	}
+	if ((*s1 != '\0') && (*s2 != '\0'))
 		return (0);
+	else if (*s1 > *s2)
+		return ();
 	else
-		return (len1 - len2);
+		return();
 }
