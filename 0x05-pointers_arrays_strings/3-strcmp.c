9fe4a485122f3ecc1032d81c79a5196bd829da3a
Athena Deng <539@holbertonschool.com>
2018-09-28 18:57:22 +0000

    removed extra ', changed if statement

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 == *s2) && (*s1 != '\0'') && (s2 != '\0'))
+	while ((*s1 == *s2) && (*s1 != '\0') && (s2 != '\0'))
 	{
 		s1++;
 		s2++;
 	}
-	if ((*s1 != '\0') && (*s2 != '\0'))
-		return (0);
-	else if (*s1 > *s2)
-		return ();
+	if ((*s1 != '\0') || (*s2 != '\0'))
+		return (s1 - s2);
 	else
-		return();
+		return (0);
 }
