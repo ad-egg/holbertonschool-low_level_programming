19a375c9229b69d2013a011b7f3ae6262398e454
Athena Deng <539@holbertonschool.com>
2018-09-28 03:25:35 +0000

    incomplete work

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes the first letter of each word
+ *
+ * @str: first letter of each word in this string will be capitalized
+ *
+ * Return: pointer to string
+ */
+
+char *cap_string(char *str)
+{
+	int ind;
+	char *fircaps = str;
+
+	ind = 0;
+
+	while (str[ind] != '\0')
+	{
+		
+
+
+
+
+
+		ind++;
+
+
+
+
+
+
+	str[0] = str[0] - 32;
+
+	return (fircaps);
+}
