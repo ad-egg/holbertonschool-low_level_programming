86d09f230e7fc24eb0ee9bbcb76f2d0d3cacf188
Athena Deng <539@holbertonschool.com>
2018-09-28 01:58:51 +0000

    shortened line that was over 80 characters

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: this string will be compared to s2
  * @s2: this string will be compared to s1
  *
- * Return: 0 if s1 and s2 are equal, otherwise print the difference of (*s1 - *s2)
+ * Return: 0 if s1 and s2 are equal, else print the difference of (*s1 - *s2)
  */
 
 int _strcmp(char *s1, char *s2)
