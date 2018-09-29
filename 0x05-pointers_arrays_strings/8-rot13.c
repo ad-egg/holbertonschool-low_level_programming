a5916c9a6bb483dff037a01dba46b125dfb4f9a5
Athena Deng <539@holbertonschool.com>
2018-09-29 03:21:21 +0000

    removed else if statements

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -11,22 +11,21 @@
 char *rot13(char *str)
 {
 	int ind;
-	char *pt = str;
+	int swap;
+
+	char old[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
 	ind = 0;
+	swap = 0;
 
-	while (str[ind] != '\0')
+	for (ind = 0; str[ind] != '\0'; ind++)
 	{
-		if ((str[ind] >= 'a') && (str[ind] <= 'm'))
-			str[ind] = str[ind] + 13;
-		else if ((str[ind] >= 'n') && (str[ind] <= 'z'))
-			str[ind] = str[ind] - 13;
-		else if ((str[ind] >= 'A') && (str[ind] <= 'M'))
-			str[ind] = str[ind] + 13;
-		else if ((str[ind] >= 'N') && (str[ind] <= 'Z'))
-			str[ind] = str[ind] - 13;
-		ind++;
+		for (swap = 0; old[swap] != '\0'; swap++)
+		{
+			if (str[ind] == old[swap])
+				str[ind] = rot[swap];
+		}
 	}
-
-	return (pt);
+	return (str);
 }
