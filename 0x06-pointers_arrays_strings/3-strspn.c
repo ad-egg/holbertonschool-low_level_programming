0978c286e496eab636b32dfde3850856bae36cb8
Athena Deng <539@holbertonschool.com>
2018-10-01 23:11:06 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ *
+ * @s: substring in s gets measured
+ * @accept: substring
+ *
+ * Return: number of bytes in the initial segment of s
+ * which consists only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				len++;
+			}
+			accept++;
+		}
+		if (*accept == '\0')
+			return (len);
+		s++;
+	}
+	return (len);
+}
