4ac62764fdce4597b4b74edde7e8688413535e14
Athena Deng <539@holbertonschool.com>
2018-10-01 23:27:39 +0000

    defined null macro

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#define NULL ((char *)0)
 #include "holberton.h"
 
 /**
@@ -12,7 +13,7 @@
 
 char *_strchr(char *s, char c)
 {
-	while ((*s != '\0') || (*s != c))
+	while ((*s != '\0') && (*s != c))
 		s++;
 	if (*s == c)
 		return (s);
