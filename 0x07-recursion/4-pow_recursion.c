a33c2d719f9980c0a74c2a08c32c2e3e6c77c915
Athena Deng <539@holbertonschool.com>
2018-10-03 21:54:12 +0000

    first commit

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - raises x to the power of y
+ *
+ * @x: number that will be raised
+ * @y: x will be raised to this power
+ *
+ * Return: value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x,y - 1));
+}
