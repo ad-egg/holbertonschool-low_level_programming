abd56d223454b7b491b95b4c1d934c337b5f3300
Athena Deng <539@holbertonschool.com>
2018-10-03 21:12:35 +0000

    first commit

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ *
+ * @s: string to be printed in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
