d9ac073215f61fb23c87fe4582009e3e4186ad21
Athena Deng <539@holbertonschool.com>
2018-10-03 03:21:30 +0000

    changed loop

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -15,22 +15,17 @@ char *_strstr(char *haystack, char *needle)
 	int x;
 	int y;
 
-	if (!*needle)
+	if (*needle == '\0')
 		return (haystack);
 	if (*needle == 0)
 		return (haystack);
 	for (x = 0; haystack[x] != '\0'; x++)
 	{
-		for (y = 0; needle[y] != '\0'; y++)
-		{
-			if (haystack[x] == needle[y])
-			{
-				if (haystack[x + 1] == needle[y + 1])
-				{
-					return (&haystack[x]);
-				}
-			}
-		}
+		y = 0;
+		while (haystack [x + y] == needle[y])
+			y++;
+		if (needle[y] == '\0')
+			return (&haystack[x]);
 	}
 	return (NULL);
 }
