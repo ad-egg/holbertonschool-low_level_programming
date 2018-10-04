f58e48147a9ab8c919a19e8b7795014c89e8e258
Athena Deng <539@holbertonschool.com>
2018-10-04 02:37:48 +0000

    incomplete work

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number whose square root will be returned
+ *
+ * Return: natural square root of n,
+ * if n does not have a natural square root, return -1
+ */
+
+int checksqrt(int n, int i);
+
+int _sqrt_recursion(int n)
+{
+	int result;
+
+	result = ;
+
+	if ((n == 0) || (n == 1))
+		return (n);
+	else if (n < 0)
+		return (-1);
+	CALL OTHER FUNCTION 
+		return (result);
+	return (-1);
+}
+
+/**
+ * checksqrt - checks for natural square root of a number
+ *
+ * @n: number who will be checked for natural square root
+ * @i: variable used to test for whether n has natural square root
+ *
+ * Return: 
+ */
+
+int checksqrt(int n, int i)
+{
+
+}
+
