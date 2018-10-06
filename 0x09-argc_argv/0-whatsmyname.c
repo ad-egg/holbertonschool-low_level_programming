4f9c0801e8c029d25928fde48be1f8c09780c6cf
Athena Deng <539@holbertonschool.com>
2018-10-06 01:08:10 +0000

    first commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ *
+ * @argc: count of arguments supplied to the program
+ * @argv: pointers to the strings of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
