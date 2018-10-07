c6e48205ae801e0ff6d5e9c3bdd1fccf0e0d25e4
Athena Deng <539@holbertonschool.com>
2018-10-07 23:37:38 +0000

    removed function

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int isnum(int j, char *s);
+#include <ctype.h>
 
 /**
  * main - multiplies two numbers
@@ -12,52 +12,27 @@ int isnum(int j, char *s);
  * return 1 if one of the numbers contains symbols that are not digits
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int ans;
-	int i;
-	int isnumber;
+	int ans = 0;
+	int i = 1;
+	int j = 0;
 
-	i = 1;
-	ans = 0;
-
-	if (argc == 1)
-		printf("0\n");
 	while (i < argc)
 	{
-		isnumber = isnum(0, argv[i]);
-
-		if (isnumber == 0)
+		while (argv[i][j] != '\0')
 		{
-			ans += atoi(argv[i]);
+			j = 0;
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
 		}
-		else if (isnumber == 1)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		ans += atoi(argv[i]);
 		i++;
 	}
 	printf("%i\n", ans);
-	return (0);
-}
-
-/**
- * isnum - checks if a symbol is a digit or not
- *
- * @j: index of string
- * @s: string to be checked for symbols that are not digits
- *
- * Return: 0 if string contains only digits, 1 if not
- */
-
-int isnum(int j, char *s)
-{
-	while (s[j] != '\0')
-	{
-		if ((s[j] < 48) && (s[j] > 57))
-			return (1);
-		j++;
-	}
 	return (0);
 }
