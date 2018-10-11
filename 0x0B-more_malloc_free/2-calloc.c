daa53adbdd4dbd9d11de9049a00632665ed69d2a
Athena Deng <539@holbertonschool.com>
2018-10-11 23:22:09 +0000

    typecasted malloc

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
-	pt = malloc(nmemb * size + 1);
+	pt = (char *)malloc(nmemb * size + 1);
 	if (pt == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
