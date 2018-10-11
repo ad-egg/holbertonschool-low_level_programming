ab8eb7a6231bbafa1e9e126c7d129b1e05467690
Athena Deng <539@holbertonschool.com>
2018-10-11 00:14:02 +0000

    changed to pointer, added new line

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -3,35 +3,41 @@
 #include <stdio.h>
 
 /**
+ * argstostr - concatenates all the arguments of program
  *
+ * @ac: number of arguments
+ * @av: pointer to arguments
  *
+ * Return: pointer to new string or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
 	int i, j, len, ptlen;
-	char **pt;
+	char *pt;
 	len = 0;
+	ptlen = 0;
 
 	if (ac == 0)
 		return (NULL);
 	else if (av == 0)
 		return (NULL);
-
 	for (i = 0; av[i] != '\0'; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 	}
-
-	pt = (char *)malloc(len * sizeof(char) + ac + 1);
+	pt = malloc(sizeof(char) * (ac + len + 1));
 	if (pt == NULL)
 		return (NULL);
-
 	for (i = 0; av[i] != '\0'; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
+		{
 			pt[ptlen] = av[i][j];
+			ptlen++;
+		}
+		pt[ptlen] = '\n';
 		ptlen++;
 	}
 	pt[ptlen] = '\0';
