a59633c71252fd3d68193b703aca5f6f7584e539
Athena Deng <539@holbertonschool.com>
2018-10-12 23:14:39 +0000

    removed trailing whitespace

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (newptr == NULL)
 		return (NULL);
 	if (ptr != NULL)
-	{	
+	{
 		for (i = 0; (i < old_size) && (i < new_size); i++)
 			newptr[i] = ((char *)ptr)[i];
 		free(ptr);
