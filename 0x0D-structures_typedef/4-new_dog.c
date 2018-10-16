80ce6f84e1bb57019262c4488f1fba71f8675623
Athena Deng <539@holbertonschool.com>
2018-10-16 00:36:12 +0000

    added line

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -28,7 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	doggo->name = malloc(sizeof(doggo->name) * namelen);
 	if (doggo->name == NULL)
-	{	free(doggo);
+	{
+		free(doggo);
 		return (NULL);
 	}
 	for (i = 0; i < namelen; i++)
