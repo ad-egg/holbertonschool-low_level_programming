68cbf955a20891780c12d6883d4ffa259adc8ad0
Athena Deng <539@holbertonschool.com>
2018-10-17 21:08:42 +0000

    added space

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -8,7 +8,7 @@
  * @f: pointer to function that prints name
  */
 
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
 	if ((name == NULL) || (f == NULL))
 		return;
