aa8260b1b21e9657fee9ac8e431aafe172eecbc5
Athena Deng <539@holbertonschool.com>
2018-10-19 22:16:48 +0000

    first commit

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns sum of all its parameters
+ *
+ * @n: number of arguments passed
+ *
+ * Return: sum of all the parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start (ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg (ap, int);
+
+	va_end (ap);
+	return (sum);
+}
