4180aa543993d5a9c6648514dd0e605f131fc34b
Athena Deng <539@holbertonschool.com>
2018-10-20 22:00:07 +0000

    removed struct declaration

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,18 +1,5 @@
 #ifndef VARIADICFUNCS_H
 #define VARIADICFUNCS_H
-
-/**
- * struct datatypes - for data types
- *
- * @car: character to print
- * @f: pointer to function that selects what type of data it is
- */
-typedef struct datatypes
-{
-	char *car;
-	void(*f)(var_list);
-} var_types;
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
