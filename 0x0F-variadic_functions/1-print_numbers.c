2c855d8fb34acd0d3edbf730f268d77c45c6ef59
Athena Deng <539@holbertonschool.com>
2018-10-20 00:02:38 +0000

    removed space between function name and open parenthesis

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list strlist;
 	unsigned int i;
 
-	va_start (strlist, n);
+	va_start(strlist, n);
 	for (i = 0; i < n; i++)
 	{
 		va_arg(strlist, char *);
