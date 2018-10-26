f17d934e35038ff75cadcaa31d7b1d9da4634616
Athena Deng <539@holbertonschool.com>
2018-10-26 03:00:12 +0000

    resubmit file, removed limits header

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to list
+ * Return: number of nodes in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (i);
+}
+
