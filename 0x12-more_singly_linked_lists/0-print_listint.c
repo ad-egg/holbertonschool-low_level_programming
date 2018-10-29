74cd4132d90dd3dadb91b298c66c8a91bf565e2e
Athena Deng <539@holbertonschool.com>
2018-10-29 20:48:43 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: points to a listint_t list
+ * Return: number of nodes in list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nonodes;
+
+	if (h == NULL)
+		nonodes = 0;
+	for (nonodes = 0; h != NULL; nonodes++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (nonodes);
+}
