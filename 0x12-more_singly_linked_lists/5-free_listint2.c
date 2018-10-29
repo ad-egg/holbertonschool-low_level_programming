7238871771b44ab98aee2b753461df77fd406743
Athena Deng <539@holbertonschool.com>
2018-10-29 23:09:31 +0000

    correctly dereferenced head pointer

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		freethis = *head;
 		free(freethis);
-		head = head->next;
+		*head = (*head)->next;
 	}
 	head = NULL;
 }
