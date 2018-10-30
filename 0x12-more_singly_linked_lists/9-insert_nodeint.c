2bad0e2f11e1e67309429fad009751b37e0753ab
Athena Deng <539@holbertonschool.com>
2018-10-30 22:03:17 +0000

    malloc'd for new node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: points at beginning of list
  * @idx: index of list where the new node should be added
- * @n: data to be put into new node 
+ * @n: data to be put into new node
  * Return: address of new node or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -23,7 +23,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (nonodes < idx)
 		return (NULL);
 	findidx = *head;
-	for (nonodes = 0; nonodes < idx; nonodes ++)
+	insert = malloc(sizeof(listint_t));
+	if (insert == NULL)
+		return (NULL);
+	for (nonodes = 0; nonodes < idx; nonodes++)
 		findidx = findidx->next;
 	findidx = insert;
 	insert->n = n;
