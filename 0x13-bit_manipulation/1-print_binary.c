38ec345d233fb57306f2c3702730c7d9bb0a7203
Athena Deng <539@holbertonschool.com>
2018-11-02 03:57:33 +0000

    incomplete work

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be converted into binary
+ */
+void print_binary(unsigned long int n)
+{
+	
+}
