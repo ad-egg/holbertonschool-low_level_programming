958763060491842fd0977db53e0ff9ee90045682
Athena Deng <539@holbertonschool.com>
2018-11-02 04:13:41 +0000

    incomplete work

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - returns the number of bits you need to flip to get from n to m
+ * @n: first integer
+ * @m: second integer
+ * Return: number of bits you need to flip, or 0 if big endian, or 1 if little
+ * endian
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
