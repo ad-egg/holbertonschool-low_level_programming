7e2c04445a3bb4710db4f91bac9475088c247905
Athena Deng <539@holbertonschool.com>
2018-11-07 04:32:13 +0000

    condensed code to not exceed length limit

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,43 +14,31 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	bread = read(file_from, buff, 1024);
+	if ((file_from == -1) || (bread == -1))
 	{
 		printf("Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		printf("Error: Can't write to %s\n", argv[2]);
-		exit (99);
-	}
-	bread = read(file_from, buff, 1024);
-	if (bread == -1)
-	{
-		printf("Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
 	bwritten = write(file_to, buff, bread);
-	if (bwritten == -1)
+	if ((file_to == -1) || (bwritten == -1))
 	{
 		printf("Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
 	close_from = close(file_from);
 	close_to = close(file_to);
-	if (close_from == -1) 
+	if ((close_from == -1) || (close_to == -1))
 	{
-		printf("Error: Can't close fd %i\n", file_from);
-		exit (100);
-	}
-	if (close_to == -1)
-	{
-		printf("Error: Can't close fd %i\n", file_to);
-		exit (100);
+		if (close_from == -1)
+			printf("Error: Can't close fd %i\n", file_from);
+		if (close_to == -1)
+			printf("Error: Can't close fd %i\n", file_to);
+		exit(100);
 	}
 	return (0);
 }
