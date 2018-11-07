6b480a843f7b736262497f337f460e1afefc8121
Athena Deng <539@holbertonschool.com>
2018-11-07 06:01:45 +0000

    added definition

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#define _GNU_SOURCE
+#include <stdio.h>
 
 /**
  * main - copies the content of a file to another file
@@ -13,21 +15,21 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(fd[2], "Usage: cp file_from file_to\n");
+		dprintf("Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	bread = read(file_from, buff, 1024);
 	if ((file_from == -1) || (bread == -1))
 	{
-		dprintf(fd[2], "Error: Can't read from file %s\n", argv[1]);
+		dprintf("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	bwritten = write(file_to, buff, bread);
 	if ((file_to == -1) || (bwritten == -1))
 	{
-		dprintf(fd[2], "Error: Can't write to %s\n", argv[2]);
+		dprintf("Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	close_from = close(file_from);
@@ -35,9 +37,9 @@ int main(int argc, char *argv[])
 	if ((close_from == -1) || (close_to == -1))
 	{
 		if (close_from == -1)
-			dprintf(fd[2], "Error: Can't close fd %i\n", file_from);
+			dprintf("Error: Can't close fd %i\n", file_from);
 		if (close_to == -1)
-			dprintf(fd[2], "Error: Can't close fd %i\n", file_to);
+			dprintf("Error: Can't close fd %i\n", file_to);
 		exit(100);
 	}
 	return (0);
