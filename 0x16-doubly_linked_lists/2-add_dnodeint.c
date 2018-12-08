de101597a42c835c43191dbb7e7cad773f8f328a
Athena Deng <539@holbertonschool.com>
2018-12-08 22:43:36 +0000

    added condition to check if linked list exists

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -14,10 +14,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	temp = *head;
-	new->n = n;
-	new->prev = NULL;
-	new->next = temp;
-	temp->prev = new;
+	if (temp == NULL)
+		new->next = NULL;
+	else
+	{
+		new->next = temp;
+		temp->prev - new;
+	}
 	*head = new;
 	return (new);
 }
