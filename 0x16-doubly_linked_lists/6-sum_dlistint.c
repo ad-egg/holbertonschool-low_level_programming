debcda942c8dbcaf56cc82a3b7c5478356692e94
Athena Deng <539@holbertonschool.com>
2018-12-08 03:00:40 +0000

    first commit

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list
+ * @head: pointer to beginning of linked list
+ * Return: sum of all the data in linked list or 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp)
+		sum += temp->n;
+	return (sum);
+}
