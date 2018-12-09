56f4435a3712b2865e169919332284f74fd402d0
Athena Deng <539@holbertonschool.com>
2018-12-09 05:04:10 +0000

    first commit

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at a given index
+ * @head: points at the beginning of a linked list
+ * @index: node at this index is to be deleted
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *temp, *current = *head, *temp2;
+
+	if (current == NULL)
+		return (-1);
+	if (idx == 0)
+	{
+		if (current->next != NULL)
+		{
+			temp = current->next;
+			temp->prev = NULL;
+			*head = temp;
+		}
+		free(current);
+		return (1);
+	}
+	for (i = 0; (i < idx) && (current->next); i++)
+		current = current->next;
+	temp = current->prev;
+	if (i == idx)
+	{
+		temp2 = current->next;
+		temp->next = temp2;
+		temp2->prev = temp;
+	}
+	else if ((i + 1) == idx)
+		temp->next = NULL;
+	else
+		return (-1);
+	free(current);
+	return (1);
+}
