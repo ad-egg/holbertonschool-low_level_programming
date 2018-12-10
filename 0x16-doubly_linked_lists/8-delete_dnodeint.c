3454ede9ee48904c8eb5c5fe500a59ac7cfd9f12
Athena Deng <539@holbertonschool.com>
2018-12-10 01:37:06 +0000

    nested if condition inside if condition

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,34 +9,33 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
-	dlistint_t *temp, *current = *head, *temp2;
+	dlistint_t *current = *head;
 
-	if (current == NULL)
+	if ((current == NULL) || (head == NULL))
 		return (-1);
 	if (index == 0)
 	{
+		*head = current->next;
 		if (current->next != NULL)
-		{
-			temp = current->next;
-			temp->prev = NULL;
-			*head = temp;
-		}
-		free(current);
-		return (1);
+			current->next->prev = NULL;
 	}
-	for (i = 0; (i < index) && (current->next); i++)
-		current = current->next;
-	temp = current->prev;
-	if (i == index)
+	else if (index > 0)
 	{
-		temp2 = current->next;
-		temp->next = temp2;
-		temp2->prev = temp;
+		for (i = 0; (i < index) && (current != NULL); i++)
+			current = current->next;
+		if (i == index)
+		{
+			if (current->next == NULL)
+				current->prev->next = NULL;
+			else
+			{
+				current->prev->next = current->next;
+				current->next->prev = current->prev;
+			}
+		}
+		else
+			return (-1);
 	}
-	else if ((i + 1) == index)
-		temp->next = NULL;
-	else
-		return (-1);
 	free(current);
 	return (1);
 }
