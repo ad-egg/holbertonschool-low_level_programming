c002fa1e3bd9fd965a9abfdf7755355221f0659c
Athena Deng <539@holbertonschool.com>
2018-12-10 04:26:05 +0000

    added blank line after declarations

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	char *str;
 	int i;
-	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	write(STDERR_FILENO, str, i);
