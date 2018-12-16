30e2613762dace957e2e77eee0cd3d8f756459b3
Athena Deng <539@holbertonschool.com>
2018-12-16 23:55:45 +0000

    removed unnecessary curly braces and updated comments

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-	{
-		printf("0123456789\n");
-	}
-
+	printf("0123456789\n");
 	return (0);
 }
