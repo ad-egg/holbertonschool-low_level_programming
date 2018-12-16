972070bd8f717c4f871dfa300a00d5ad54f3d41a
Athena Deng <539@holbertonschool.com>
2018-12-16 23:59:25 +0000

    removed unnecessary curly braces and updated comments

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int no;
 
-	for (no = '0' ; no <= '9' ; no++)
-	{
-		putchar(no);
-	}
+	for (no = 0; no < 10; no++)
+		putchar(no + '0');
 	putchar ('\n');
-
 	return (0);
 }
