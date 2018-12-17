91920ed87cb13bce79d8615851f44e1adc73a544
Athena Deng <539@holbertonschool.com>
2018-12-17 02:39:53 +0000

    added variables and made code much shorter

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,45 +1,31 @@
 #include "holberton.h"
 
 /**
- * times_table - Entry point
+ * times_table - prints the 9 times table starting with 0
  */
 
 void times_table(void)
 {
-	int hor;
-	int ver;
+	int hor, ver, no, no1, no2;
 
-	for (ver = 0; ver <= 9; ver++)
+	for (ver = 0; ver < 10; ver++)
 	{
-		for (hor = 0; hor <= 9; hor++)
+		for (hor = 0; hor < 10; hor++)
 		{
-			if (hor == 0)
-			{
-				_putchar((ver * hor) + '0');
+			no = hor * ver;
+			no1 = no / 10;
+			no2 = no % 10;
+			if (hor > 0)
+				_putchar(' ');
+			if ((no < 10) && (hor > 0))
+				_putchar(' ');
+			else if (no > 9)
+				_putchar(no1 + '0');
+			_putchar(no2 + '0');
+			if (hor < 9)
 				_putchar(',');
-				_putchar(' ');
-			}
-			else if ((ver * hor) <= 9)
-			{
-				_putchar(' ');
-				_putchar((ver * hor) + '0');
-				if (hor < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar(((ver * hor) / 10) + '0');
-				_putchar(((ver * hor) % 10) + '0');
-				if (hor < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			else if (hor == 9)
+				_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
