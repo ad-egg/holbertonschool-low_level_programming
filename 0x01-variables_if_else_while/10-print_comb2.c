03b1da2c20d68c3cafee8ec160deaa7e439a8abf
Athena Deng <539@holbertonschool.com>
2018-12-17 00:07:46 +0000

    removed unnecessary curly braces, updated comments

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints the numbers from 00 to 99
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int no;
 
-	for (no = 0 ; no < 100 ; no++)
+	for (no = 0; no < 100; no++)
 	{
 		putchar((no / 10) + '0');
 		putchar((no % 10) + '0');
-		if (no != 99)
+		if (no < 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar ('\n');
-
 	return (0);
 }
