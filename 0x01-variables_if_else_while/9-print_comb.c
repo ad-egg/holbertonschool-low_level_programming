17bdfead4e3365c2b63d2cea8fade76990aadba7
Athena Deng <539@holbertonschool.com>
2018-12-17 00:06:13 +0000

    removed unnecessary curly braces, changed variable type, updated comments

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints all possible combination of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int no;
 
-	for (no = '0' ; no <= '9' ; no++)
+	for (no = 0; no < 10; no++)
 	{
-		putchar(no);
-
-		if (no < '9')
+		putchar(no + '0');
+		if (no < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar ('\n');
-
 	return (0);
 }
