d7a73378d2719ceb23035d03824d89fcaf958b04
Athena Deng <539@holbertonschool.com>
2018-12-17 01:40:11 +0000

    updated comments, added variable

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,28 @@
 #include "holberton.h"
 
 /**
- * print_sign - Entry point
- *
+ * print_sign - prints the sign of an integer
  * @n: this variable is checked for its sign
- *
- * Return: Always 0 (Success)
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is negative
  */
 
 int print_sign(int n)
 {
+	int val;
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
-		_putchar('\n');
+		val = 1;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
-		_putchar('\n');
+		val = -1;
 	}
-	else
+	else if (n == 0)
 	{
-		_putchar('0');
-		return (0);
-		_putchar('\n');
+		_putchar(n + '0');
+		val = 0;
 	}
-
-	return (0);
+	return (val);
 }
