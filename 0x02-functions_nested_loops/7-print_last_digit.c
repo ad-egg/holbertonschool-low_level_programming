16235bc7b6db0d44d8ebcadd975dd0e9a30e0c3d
Athena Deng <539@holbertonschool.com>
2018-12-17 02:08:57 +0000

    added variable

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,27 +1,20 @@
 #include "holberton.h"
 
 /**
- * print_last_digit - Entry point
- *
+ * print_last_digit - prints the last digit of n
  * @n: this variable is the number whose last digit will be checked
- *
  * Return: the last digit of int n
  */
 
 int print_last_digit(int n)
 {
-	int zz;
+	int zz, n2;
 
-	zz = n % 10;
-
-	if (n > 0)
-	{
-		_putchar(zz + '0');
-		return (zz);
-	}
+	if (n >= 0)
+		n2 = n;
 	else
-	{
-		_putchar((zz * -1) + '0');
-		return (zz * -1);
-	}
+		n2 = n * -1;
+	zz = n2 % 10;
+	_putchar(zz + '0');
+	return (zz);
 }
