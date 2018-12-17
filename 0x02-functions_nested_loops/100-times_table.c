bc4bc7a7b97720e77972e6c91cb7c1bcb18bda52
Athena Deng <539@holbertonschool.com>
2018-12-17 03:38:53 +0000

    added variables and shortened code, updated comments

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,63 +1,39 @@
 #include "holberton.h"
 
 /**
- * times_table - Entry point
- *
+ * print_times_table - prints the n times table starting from 0
  * @n: this number has to be a value between 0 and 15 to display a times table
  */
 
 void print_times_table(int n)
 {
-	int hor;
-	int ver;
+	int hor, ver, no, no1, no2, no3;
 
-	if ((n > 0) && (n <= 15))
+	if ((n < 0) || (n > 15))
+		return;
+	for (ver = 0; ver <= n; ver++)
 	{
-		for (ver = 0; ver <= n; ver++)
+		for (hor = 0; hor <= n; hor++)
 		{
-			for (hor = 0; hor <= n; hor++)
-			{
-				if (hor == 0)
-				{
-					_putchar((ver * hor) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if ((ver * hor) <= 9)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((ver * hor) + '0');
-					if (hor < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else if ((ver  * hor) <= 99)
-				{
-					_putchar(' ');
-					_putchar(((ver * hor) / 10) + '0');
-					_putchar(((ver * hor) % 10) + '0');
-					if (hor < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar(((ver * hor) / 100) + '0');
-					_putchar((((ver * hor) / 10) % 10) + '0');
-					_putchar(((ver * hor) % 10) + '0');
-					if (hor < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-			}
-			_putchar('\n');
+			no = hor * ver;
+			no1 = no / 100;
+			no2 = (no / 10) % 10;
+			no3 = no % 10;
+			if (hor > 0)
+				_putchar(' ');
+			if ((no < 100) && (hor > 0))
+				_putchar(' ');
+			else if (no > 99)
+				_putchar(no1 + '0');
+			if ((no < 10) && (hor > 0))
+				_putchar(' ');
+			else if (no > 9)
+				_putchar(no2 + '0');
+			_putchar(no3 + '0');
+			if (hor < n)
+				_putchar(',');
+			else if (hor == n)
+				_putchar('\n');
 		}
 	}
 }
