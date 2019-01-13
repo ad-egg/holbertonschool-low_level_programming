2dba166089939dad625ae11754d3ae0d651397b4
Athena Deng <539@holbertonschool.com>
2019-01-13 00:19:58 +0000

    first commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int reps;
+	unsigned long int fib, num1 = 0, num2 = 1;
+
+	for (reps = 1; reps <= 98; reps++)
+	{
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+		if (reps < 50)
+			printf("%lu, ", fib);
+		else
+			printf("%lu\n", fib);
+	}
+	return (0);
+}
