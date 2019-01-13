7eb573f5443a5eac412b581afce3bbbc98ae9094
Athena Deng <539@holbertonschool.com>
2019-01-13 00:04:24 +0000

    first commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even numbers in the Fibonacci
+ * sequence whose values do not exceed 4,000,000
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	unsigned long int fib = 0, num1 = 0, num2 = 1, total = 0;
+
+	while(fib < 4000000)
+	{
+		if (fib % 2 == 0)
+			total += fib;
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+	}
+	printf("%lu\n", total);
+	return (0);
+}
