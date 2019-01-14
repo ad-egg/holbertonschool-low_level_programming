c087d917590f2d5b1ed720dda1222c4cbfeed294
Athena Deng <539@holbertonschool.com>
2019-01-14 23:20:03 +0000

    included standard libraries

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
