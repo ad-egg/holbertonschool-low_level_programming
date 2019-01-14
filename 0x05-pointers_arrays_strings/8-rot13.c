8d08e62825636f16c1abed6ecb0fcba5fc924078
Athena Deng <539@holbertonschool.com>
2019-01-14 00:51:56 +0000

    return new array, used one loop with nested if statements

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,43 +1,35 @@
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
  * rot13 - encodes a string using rot13
- *
  * @str: this string will be encoded
- *
  * Return: pointer to string
  */
 
 char *rot13(char *str)
 {
-	int ind;
-	int swap;
+	int i, length;
+	char *newstr;
 
-	char old[] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot[] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A',
-		'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-		'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-		'h', 'i', 'j', 'k', 'l', 'm'};
-
-	ind = 0;
-	swap = 0;
-
-	for (ind = 0; str[ind] != '\0'; ind++)
+	for (length = 0; str[length] != '\0'; length++)
+		;
+	newstr = malloc(length);
+	if (newstr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (swap = 0; old[swap] != '\0'; swap++)
+		if (((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a'
+			) && (str[i] <= 'z')))
 		{
-			if (str[ind] == old[swap])
-				str[ind] = rot[swap];
+			if (((str[i] >= 'A') && (str[i] <= 'M')) || ((
+				str[i] >= 'a') && (str[i] <= 'm')))
+				newstr[i] = str[i] + 13;
+			else
+				newstr[i] = str[i] - 13;
 		}
+		else
+			newstr[i] = str[i];
 	}
-	return (str);
+	return (newstr);
 }
