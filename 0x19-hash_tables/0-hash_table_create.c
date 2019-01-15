850a3b72598500590039395b04cdb457aefea41a
Athena Deng <539@holbertonschool.com>
2019-01-15 01:26:52 +0000

    added variable

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,16 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable;
+	hash_node_t *hashnode;
 
 	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 		return (NULL);
 	hashtable->size = size;
-	hashtable->array = malloc(size);
-	if (hashtable->array == NULL)
+	hashnode = malloc(size);
+	if (hashnode == NULL)
 	{
 		free(hashtable);
 		return (NULL);
 	}
+	hashtable->array = &hashnode;
 	return (hashtable);
 }
