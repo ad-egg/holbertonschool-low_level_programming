2196dcb27a1e6ee9abcade740070c28b1f80dfa3
Athena Deng <539@holbertonschool.com>
2019-01-16 02:01:09 +0000

    checked if key pointer points to NULL

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -14,6 +14,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL)
 		return (NULL);
+	if (key == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 	while (temp != NULL)
