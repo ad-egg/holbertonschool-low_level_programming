fa98c3ac4111a8f655320a99dff42cd94f2e3f42
Athena Deng <539@holbertonschool.com>
2019-01-16 02:03:51 +0000

    checked for if key is empty string

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,9 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp = NULL;
 
-	if (ht == NULL)
+	if ((ht == NULL) || (key == NULL))
 		return (NULL);
-	if (key == NULL)
+	if (strlen(key) == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
