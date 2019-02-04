bc2a988b4ad6cf35d8112118fa635fb63ea5e75b
Athena Deng <539@holbertonschool.com>
2019-02-04 04:19:26 +0000

    incomplete work

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using quick sort
+ * method
+ * @array: pointer to array of integers
+ * @size: size of array of integers
+ */
+void quick_sort(int *array, size_t size)
+{
+	if ((array == NULL) || (size <= 1))
+		return;
+}
