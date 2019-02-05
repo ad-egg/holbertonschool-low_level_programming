78e3642ed92b6e0e9b223fb5068dc267553f5c24
Athena Deng <539@holbertonschool.com>
2019-02-05 02:43:53 +0000

    incomplete work

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -11,3 +11,33 @@ void quick_sort(int *array, size_t size)
 	if ((array == NULL) || (size <= 1))
 		return;
 }
+
+
+
+int pivot = size - 1, end = size - 1, i = -1, j = 0, temp;
+
+while (j < end)
+{
+	if (array[pivot] > array[j])
+	{
+		i++;
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = array[i];
+	}
+	j++;
+	if (j == end - 1)
+	{
+/* need insert the pivot into index i+1 */
+		k = i + 1;
+		temp = array[pivot];
+		pivot = temp;
+/* now pivot holds pivot value, loop shifts everything after i+1 */
+		while (j < k)
+		{
+
+
+
+		}
+	}
+}
