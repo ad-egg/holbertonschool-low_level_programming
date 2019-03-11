f3531d0e408f3ffc1c2850cf3436b92d3c1f19c7
Athena Deng <539@holbertonschool.com>
2019-03-11 21:17:50 +0000

    added pointers, recursive function

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -6,8 +6,25 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return;
+	binary_tree_t *left_child;
+	binary_tree_t *right_child;
 
-
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+		{
+			left_child = tree->left;
+			tree->left = NULL;
+		}
+		if (tree->right != NULL)
+		{
+			right_child = tree->right;
+			tree->right = NULL;
+		}
+		left_child->parent = NULL;
+		right_child->parent = NULL;
+		free(tree);
+		binary_tree_delete(left_child);
+		binary_tree_delete(right_child);
+	}
 }
