ecef9b665fd5d8caa604ec287017529e8bc188bb
Athena Deng <539@holbertonschool.com>
2019-03-11 22:33:43 +0000

    corrected typo of variable name

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	right_child = binary_tree_node(parent, value);
-	if (rigt_child == NULL)
+	if (right_child == NULL)
 		return (NULL);
 	if (parent->right == NULL)
 		parent->right = right_child;
