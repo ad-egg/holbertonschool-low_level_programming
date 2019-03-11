fdd6ffd8211006f3f3184fb5eaed8aa8aaeab045
Athena Deng <539@holbertonschool.com>
2019-03-11 22:15:01 +0000

    changed return value according to task requiremets

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -3,12 +3,12 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
- * Return: 1 if node is a leaf, return 0 if not, return NULL if node is NULL
+ * Return: 1 if node is a leaf, return 0 if not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return (NULL);
+		return (0);
 	if ((node->left == NULL) && (node->right == NULL))
 		return (1);
 	else
