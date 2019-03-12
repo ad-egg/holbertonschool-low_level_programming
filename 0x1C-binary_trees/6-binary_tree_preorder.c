d6b8eab0e1bfcd3de1c19930e01930bbd12a957b
Athena Deng <539@holbertonschool.com>
2019-03-12 02:28:51 +0000

    incomplete work

---
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through binary tree using pre-order traversal
+ * @tree: pointer to root node of tree to traverse
+ * @func: pointer to function to call for each node
+ * @int: value in the node to be called by function
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+
+
+
+}
---
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverses a binary tree using post-order traversal
+ * @tree: pointer to root node of tree to traverse
+ * @func: pointer to function to call for each node
+ * @int: value stored in each node that is called by function
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+
+
+
+
+
+
+}
