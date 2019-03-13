4b286f584ea1328cd26e8084f61c87f8e1a9b681
Athena Deng <539@holbertonschool.com>
2019-03-13 20:50:49 +0000

    recursive function that checks if a binary tree is full

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -7,7 +7,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int n;
-
-	return (n);
+	if (tree == NULL)
+		return (0);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+	else if ((tree->left != NULL) && (tree->right != NULL))
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	else
+		return (0);
 }
