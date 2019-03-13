c0a9c0dddebeabf36076aa6aa16ea57367537d61
Athena Deng <539@holbertonschool.com>
2019-03-13 19:46:50 +0000

    removed variables

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -7,17 +7,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t rh = 0;
-	size_t lh = 0;
-
 	if (tree == NULL)	
 		return (0);
-	if (tree->left != NULL)
-		lh = binary_tree_height(tree->left) + 1;
-	if (tree->right != NULL)
-		rh = binary_tree_height(tree->right) + 1;
-	if (lh > rh)
-		return (lh);
+	if ((1 + _binary_tree_height(tree->left)) > (1 + _binary_tree_height(tree->right)))
+		return (1 + _binary_tree_height(tree->left));
 	else
-		return (rh);
+		return (1 + _binary_tree_height(tree->right));
 }
