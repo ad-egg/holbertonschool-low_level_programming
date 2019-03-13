5d3b3151825341b7d523ea8ff3f21710ca3114ab
Athena Deng <539@holbertonschool.com>
2019-03-13 02:33:24 +0000

    added helper function

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node
+ * @value: value to put in node
+ * Return: pointer to newly created node or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (parent == NULL)
+		new_node->parent = NULL;
+	else
+		new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+	return (new_node);
+}
+
+/**
  * binary_tree_insert_right - inserts a node as a right-child of another node
  * @parent: pointer to node that will receive a right-child
  * @value: value to be stored in right-child
