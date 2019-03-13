5987fce895d618b1a28a741496091c06cdbcc48f
Athena Deng <539@holbertonschool.com>
2019-03-13 02:18:07 +0000

    recursive function to count number of nodes with at least one child in a binary tree

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -7,8 +7,10 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t num_nodes = 0;
-
-
-	return (num_nodes);
+	if (tree == NULL)
+		return (0);
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (0);
+	else
+		return (1 + (binary_tree_nodes(tree->left)) + (binary_tree_nodes(tree->right)));
 }
