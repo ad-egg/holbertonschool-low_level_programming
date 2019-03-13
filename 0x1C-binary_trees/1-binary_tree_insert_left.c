af7fc4bba5365baaf8858b4e78bbadd96fe779c8
Athena Deng <539@holbertonschool.com>
2019-03-13 02:35:42 +0000

    removed helper function

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -1,29 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to parent node
- * @value: value to put in node
- * Return: pointer to newly created node or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
-	if (parent == NULL)
-		new_node->parent = NULL;
-	else
-		new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->n = value;
-	return (new_node);
-}
-
-/**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to node to insert the left-child in
  * @value: value to store in the left-child
