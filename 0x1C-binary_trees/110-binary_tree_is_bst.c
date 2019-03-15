8f623ec1136cb937c1f9bfefa8e0513c50ed3a8a
Athena Deng <539@holbertonschool.com>
2019-03-15 00:49:56 +0000

    changed value to be passed to recursive function

--- 0x1C-binary_trees/110-binary_tree_is_bst.c
+++ 0x1C-binary_trees/110-binary_tree_is_bst.c
@@ -14,7 +14,7 @@ int is_bst_recur(const binary_tree_t *tree, int min, int max)
 		return (1);
 	if ((tree->n <= min) || (tree->n >= max))
 		return (0);
-	return (is_bst_recur(tree->left, min, tree->n + 1) &&
+	return (is_bst_recur(tree->left, min, tree->n - 1) &&
 			is_bst_recur(tree->right, tree->n + 1, max));
 }
 
