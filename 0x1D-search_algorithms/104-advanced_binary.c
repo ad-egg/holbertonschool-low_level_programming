808f2e19a49515a476d3e32250420c6be91469bf
Athena Deng <539@holbertonschool.com>
2019-04-17 02:39:19 +0000

    first commit

---
+++ 0x1D-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers using
+ * binary search algorithm
+ * @array: pointer to array of integers
+ * @size: size of integer array
+ * @value: value to be found
+ * Return: index at which value can be found, or -1 if it is not in array
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%i", array[i]);
+		if (i < size - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	i = size / 2;
+	if (array[i] > value)
+		return (advanced_binary(&array[0], i, value));
+	else if (array[i] < value)
+	{
+		if (size % 2 == 1 && i > 1)
+			return (advanced_binary(&array[i + 1], i, value));
+		else if (size % 2 == 0 && i > 1)
+			return (advanced_binary(&array[i], i, value));
+		else
+			return (-1);
+	}
+	else
+		return (i);
+}
