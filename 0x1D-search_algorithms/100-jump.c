b93507c3723b544826965f4b1dbe91ab0442ac88
Athena Deng <539@holbertonschool.com>
2019-04-17 22:03:46 +0000

    first commit

---
+++ 0x1D-search_algorithms/100-jump.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in a soted array of integers using
+ * jump search algorithm
+ * @array: pointer to beginning of array of integers
+ * @size: size of array of integers
+ * @value: value to be found in array
+ * Return: index in array at which value can be found, or -1 if it's not there
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	unsigned int i, j, r;
+
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+	if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		else
+			return (-1);
+	}
+	r = sqrt(size);
+	for (i = 0; array[i] < value && i < size; i += r)
+	{
+		printf("Value checked array[%u] = [%i]\n", i, array[i]);
+		j = i;
+	}
+	printf("Value found between indexes [%u] and [%u]\n", j, i);
+	for (; j <= i && j < size; j++)
+	{
+		printf("Value checked array[%u] = [%i]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}
