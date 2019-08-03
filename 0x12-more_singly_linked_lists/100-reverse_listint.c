a6e9c1906028934c2fac511aadbab3455d8dd54b
Athena <539@holbertonschool.com>
2019-08-03 02:50:32 +0000

    replaced whitespace with tabs, added space

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -8,16 +8,16 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-        listint_t *current = *head;
-        listint_t *next;
+	listint_t *current = *head;
+	listint_t *next;
 
-        while(current != NULL)
-        {
-                next = current->next;
-                current->next = prev;
-                prev = current;
-                current = next;
-        }
-        *head = prev;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
 	return (*head);
 }
